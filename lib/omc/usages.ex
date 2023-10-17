f2740a01ce5f510abfb7fcd09037c989f23372f7
Mehdi Taghizadeh <snofang@gmail.com>
2023-10-17 11:42:26 +0330

    moving usage_state to its own module

--- lib/omc/usages.ex
+++ lib/omc/usages.ex
@@ -17,106 +17,12 @@ defmodule Omc.Usages do
   alias Omc.Servers.ServerAccUser
   alias Omc.Ledgers
   alias Omc.Ledgers.Ledger
-  alias Omc.Usages.Usage
+  alias Omc.Usages.{Usage, UsageState}
   alias Omc.Repo
   import Ecto.Query
 
   # Minimum duration in seconds which is considered in usage calculations
   @minimum_considerable_usasge_in_seconds 60 * 60
-
-  defmodule UsageState do
-    @moduledoc false
-
-    defstruct usages: [],
-              ledgers: [],
-              changesets: []
-
-    def add_usage_ledger_tx(
-          %__MODULE__{} = state,
-          %Money{} = money,
-          %{} = usage_item_attrs
-        ) do
-      state
-      |> add_changesets(money, usage_item_attrs)
-      |> apply_usage_changeset()
-      |> apply_ledger_changeset()
-    end
-
-    defp add_changesets(%__MODULE__{} = state, money, usage_item_attrs) do
-      state
-      |> add_item(
-        :changesets,
-        Ledgers.ledger_update_changeset(%{
-          ledger: ledger_by_currency(state, money.currency),
-          context: :usage,
-          context_id: -1,
-          amount: money.amount,
-          type: :debit
-        })
-        |> Map.put(:usage_item_changeset, UsageItem.create_changeset(usage_item_attrs))
-      )
-    end
-
-    defp apply_usage_changeset(%__MODULE__{} = state) do
-      %{usage_item_changeset: changeset} = state.changesets |> List.last()
-
-      state
-      |> Map.replace(
-        :usages,
-        state.usages
-        |> Enum.map(fn usage ->
-          if usage.id == changeset.changes.usage_id do
-            usage
-            |> Map.replace(
-              :usage_items,
-              usage.usage_items ++ [Ecto.Changeset.apply_changes(changeset)]
-            )
-          else
-            usage
-          end
-        end)
-      )
-    end
-
-    # defp apply_ledger_tx_changeset(%__MODULE__{} = state) do
-    #   %{ledger_tx_changeset: changeset} = state.changesets |> List.last()
-    #
-    #   state
-    #   |> apply_changeset(:ledger_tx_changesets, changeset)
-    # end
-
-    defp apply_ledger_changeset(%__MODULE__{} = state) do
-      %{ledger_changeset: changeset} = state.changesets |> List.last()
-
-      state
-      |> apply_changeset(:ledgers, changeset)
-    end
-
-    defp add_item(%__MODULE__{} = state, member, item) do
-      state
-      |> Map.replace(member, Map.get(state, member) ++ [item])
-    end
-
-    defp apply_changeset(%__MODULE__{} = state, member, changeset) do
-      state
-      |> Map.replace(
-        member,
-        Map.get(state, member)
-        |> Enum.map(fn item ->
-          if item.id == changeset.data.id do
-            Ecto.Changeset.apply_changes(changeset)
-          else
-            item
-          end
-        end)
-      )
-    end
-
-    defp ledger_by_currency(%__MODULE__{} = state, currency) do
-      state.ledgers
-      |> Enum.find(&(&1.currency == currency))
-    end
-  end
 
   @doc """
   Returns current usage state (without persisting anything) in terms of current computed last state of 
