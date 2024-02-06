67c242a68abd5d96e5a95cf5594712ee5f5d85b6
Mehdi Taghizadeh <snofang@gmail.com>
2024-02-06 05:45:20 +0330

    v0.1.9

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Omc.MixProject do
   def project do
     [
       app: :omc,
-      version: "0.1.8",
+      version: "0.1.9",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
